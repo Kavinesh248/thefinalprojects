ed47e8bbcc0dd40378886bd59929e2b899da3777
kavinesh <kavineshkavinesh860@gmail.com>
2025-04-14 14:23:28 +0530

    fullscreen

--- src/components/ProjectContent/ProjectContent.jsx
+++ src/components/ProjectContent/ProjectContent.jsx
@@ -15,10 +15,10 @@ const ProjectContent = function () {
           className="h-full w-full object-cover"
           src={embedUrl}
           title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
         ></iframe>
       </div>
 
