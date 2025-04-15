56602593d80c93df7d9aa76a2f14c73e79ee84f1
kavinesh <kavineshkavinesh860@gmail.com>
2025-04-15 16:30:20 +0530

    referral

--- src/pages/ReferralProgram.jsx
+++ src/pages/ReferralProgram.jsx
@@ -19,12 +19,12 @@ function ReferralProgram() {
 
   return (
     <div className="relative h-full bg-black text-white lg:px-10 lg:py-10">
-      <h1 className="mb-6 text-[1.8rem] font-semibold sm:mb-8 sm:text-[2rem] md:text-[2.2rem] lg:mb-10 lg:text-[2.4rem] xl:text-[2.6rem]">
+      <h1 className="mb-6 text-[1.9rem] font-semibold sm:mb-8 sm:text-[2.1rem] md:text-[2.3rem] lg:mb-10 lg:text-[2.4rem] xl:text-[2.6rem]">
         Unlock Exclusive Rewards by Joining thefinalprojects Referral Program 🚀
       </h1>
 
       <div className="mb-18">
-        <p className="mb-2 text-[1.4rem] sm:mb-3 sm:text-[1.6rem] md:text-[1.8rem] lg:text-[1.9rem]">
+        <p className="mb-3 text-[1.5rem] sm:mb-3 sm:text-[1.6rem] md:text-[1.8rem] lg:text-[1.9rem]">
           Click the button below and learn how this works
         </p>
         <button
@@ -63,7 +63,7 @@ function ReferralProgram() {
       />
 
       <form
-        className="mt-8 flex w-[28rem] flex-col gap-6 text-[1.4rem] text-white sm:w-[35rem] md:w-[40rem] lg:gap-8"
+        className="mt-8 flex w-[30rem] flex-col gap-6 text-[1.4rem] text-white sm:w-[35rem] md:w-[40rem] lg:gap-8"
         ref={formRef}
         onSubmit={handleSubmit}
       >
@@ -87,8 +87,22 @@ function ReferralProgram() {
           placeholder="Contact number or Email"
           name="contact_info"
         />
-        <button className="w-min rounded-lg bg-white px-4 py-2 font-medium text-black sm:text-[1.5rem] md:px-6 md:py-3 lg:text-[1.7rem]">
-          Submit
+        <button className="mt-3 flex w-min items-center gap-2 rounded-lg bg-white px-5 py-2 text-[1.5rem] font-medium text-black md:px-6 md:py-3 lg:text-[1.7rem]">
+          <span>Submit</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+            />
+          </svg>
         </button>
       </form>
 
