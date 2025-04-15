18e41889bfdc42de953727ea75974a8481e933a4
kavinesh <kavineshkavinesh860@gmail.com>
2025-04-15 16:03:11 +0530

    welcome message

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,9 +2,11 @@ import { assets } from "../../assets/assets";
 
 const Welcome = function () {
   return (
-    <div className="mb-18 flex items-center rounded-lg bg-[linear-gradient(288.17deg,#45108A_2.3%,#3D065F_29.86%,#10054D_101.35%)] px-8 py-6 text-center text-[2rem] font-medium text-white md:px-10 md:py-8 md:text-[2.6rem] xl:px-12 xl:py-10 xl:text-[2.4rem]">
+    <div className="mb-18 flex items-center rounded-lg bg-[linear-gradient(288.17deg,#45108A_2.3%,#3D065F_29.86%,#10054D_101.35%)] px-8 py-6 text-center text-[2rem] font-medium text-white md:px-10 md:py-8 md:text-[2.6rem] xl:text-[2.4rem]">
       <div className="flex-grow text-left">
-        <span className="md:tracking-wide">Welcome, Smart Thinker 🎉!</span>
+        <span className="md:tracking-wide lg:text-[2.6rem]">
+          Welcome, Smart Thinker 🎉!
+        </span>
         <p className="hidden text-[1.4rem] font-normal md:block md:text-[1.6rem] lg:text-[1.8rem]">
           Just one step away to get your maximum Internal points.
         </p>
