62aa7a96db71bfcb35541e0235a3d0e4506873a7
kavinesh <kavineshkavinesh860@gmail.com>
2025-04-24 00:30:24 +0530

    updated lcp image

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { preloadImage } from "./utils/preload";
 
 const queryClient = new QueryClient();
+
+preloadImage(
+  "https://ik.imagekit.io/40czoqvvk/category_thumbnail_optimized/Machine%20Learning.webp?tr=w-1280,f-auto",
+);
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
